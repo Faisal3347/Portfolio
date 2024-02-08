414310df78756cf167d082736a1629f315cb2cc9
Faisal3347 <149330357+Faisal3347@users.noreply.github.com>
2024-02-08 10:26:04 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,32 @@
+$(document).ready(function()
+{
+    $('#menu').click(function()
+    {
+        $(this).toggleClass('fa-time');
+        $('header').toggleClass('toggle');
+    });
+    $('window').on('scroll load',function()
+    {
+        $('#menu').removeClass('fa-time');
+        $('header').removeClass('toggle');
+
+        //for makin upward arrow
+        if($(window).scrollTop() > 0){
+            $('.to-top').show();
+
+        }else{
+            $('.to-top').hide();
+        }
+    });
+//smooth scroll
+ $('a[href*="#"]').click(function(e){
+    e.preventDefault();
+    $('html , body').animate({
+        scrollTop : $($(this).attr('href')).offset().top,
+    },
+    500,
+    'linear'
+    );
+ });
+
+});
